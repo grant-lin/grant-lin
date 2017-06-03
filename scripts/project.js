9a1d5ff6c620335288bf031247799cb986c92b4b
Grant Hao-Wei Lin <grant.haowei.lin@gmail.com>
2017-06-03 02:17:32 -0700

    mobile fixes for project view

--- scripts/project.js
+++ scripts/project.js
@@ -1,5 +1,20 @@
 $(document).ready(function(){
   console.log('working');
+  //check if current screen is mobile
+  var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ? true : false;
+  /*==========================================================================
+     Hamburger Menu
+  ==========================================================================*/
+
+  //why does this decrease the clickable area of hamburger-menu?
+  $('.hamburger-menu').click( function() {
+      console.log('hamburger-menu clicked');
+      event.preventDefault();
+      $('.bar').toggleClass('animate');
+      $('header nav ul').slideToggle();
+      //need to add a line to auto scroll browser to the top
+      window.scrollTo(0, 0);
+    });
   /*==========================================================================
        Individual Project Navigation
     ==========================================================================*/
@@ -25,6 +40,50 @@ $(document).ready(function(){
       $('aside.project-nav').removeClass('is-fixed');
       console.log('3');
     }
+  });
+
+    //on clicking link, smooth scroll to respective id
+  $('a[href^="#"]').click(function() {
+    event.preventDefault();
+    var _id = $(this).attr('href');
+    console.log(_id);
+    $('body, html').animate({scrollTop: $(_id).offset().top + 1}, 900, 'swing');
+  });
+
+  //adding is-active to active nav link
+  //get the array of project nav anchors
+  var projectNavAnchors = $('.project-nav').find('a');
+  //transform projectNavAnchors array to sectionNavIds Array
+  var projectNavIds = projectNavAnchors.map(function() {
+    var item = $($(this).attr("href"));
+    if (item.length) { return item; }
+  });
+  var currId;
+
+
+    //highlight current sec-#
+    $(window).scroll(function(e) {
+    //if it's not mobile browser
+    if(!isMobile) {
+      // highlight current tab
+      // Get id of current scroll item
+      var _scrolled = $(window).scrollTop();
+      var cur = projectNavIds.map(function(){
+       if ($(this).offset().top < _scrolled)
+         return this;
+      });
+      // Get the id of the current element
+       cur = cur[cur.length-1];
+       currId = cur && cur.length ? cur[0].id : "";
+       console.log ('current id is ' + currId);
+
+       // Set/remove active class
+       projectNavAnchors
+       //remove the is-active class of all the parents of the anchors (aka li's)
+       .parent().removeClass('is-active')
+       //filter the current li and add the is-active class
+       .end().filter("[href='#"+currId+"']").parent().addClass('is-active');
+     } 
   });
 
 });
