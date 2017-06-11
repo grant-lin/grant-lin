f7f4a49473f2c6d490556b6b5ac6bdaaa6209c9c
Grant Hao-Wei Lin <grant.haowei.lin@gmail.com>
2017-06-10 20:47:24 -0700

    new pages added

--- scripts/project.js
+++ scripts/project.js
@@ -23,21 +23,21 @@ $(document).ready(function(){
     console.log('_scrolled is ' + _scrolled);
     var _projectBodyTop = $('.project-body').offset().top;
     console.log('_projectBodyTop is ' + _projectBodyTop);
-    var _navHeight = $('aside.project-nav').height();
+    var _navHeight = $('nav.project-nav').height();
     console.log('_navHeight is ' + _navHeight);
     var _projectBodyBottom = $('.project-body').offset().top + $('.project-body').height() - _navHeight - 38;
     console.log('_projectBodyBottom is ' + _projectBodyBottom);
 
     if (_scrolled >= _projectBodyTop && _scrolled < _projectBodyBottom) {
-      $('aside.project-nav').removeClass('is-fixed-end').addClass('is-fixed');
-      $('aside.project-nav').fadeIn(500);
+      $('nav.project-nav').removeClass('is-fixed-end').addClass('is-fixed');
+      $('nav.project-nav').fadeIn(500);
       console.log('1');
     } else if (_scrolled >= _projectBodyBottom) {
-      $('aside.project-nav').removeClass('is-fixed').addClass('is-fixed-end');
-      $('aside.project-nav').fadeOut(500);
+      $('nav.project-nav').removeClass('is-fixed').addClass('is-fixed-end');
+      $('nav.project-nav').fadeOut(500);
       console.log('2');
     } else {
-      $('aside.project-nav').removeClass('is-fixed');
+      $('nav.project-nav').removeClass('is-fixed');
       console.log('3');
     }
   });
